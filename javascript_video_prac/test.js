8ce60008724e227677520d9b35c1ae80d7030ba5
Meastro ZI <meastro15012004@gmail.com>
2022-06-24 16:32:48 +0530

    commiting after adding project folder and some siles

---
+++ javascript_video_prac/test.js
@@ -0,0 +1,19 @@
+console.log("ya this test file is working")
+function setval(){
+
+    let test = "hello"
+
+}
+
+
+function changeval(){
+    console.log(test)
+    let test = " hello again"
+}
+
+setval();
+changeval();
+
+
+
+
